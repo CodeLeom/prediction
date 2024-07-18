f321a646c575acc7e0b1cf63b3e2acfd28eb8ea3
Ayodele Aransiola <36636824+CodeLeom@users.noreply.github.com>
2024-07-18 11:33:32 +0100

    chore: setup repo and spinner component for loading state

---
+++ src/components/layout/Spinner.jsx
@@ -0,0 +1,13 @@
+
+function Spinner() {
+    return (
+      <>
+          <div className="spinner-container">
+              <div className="custom-loader"></div>
+          </div>
+      </>
+    )
+  }
+  
+  export default Spinner
+  
